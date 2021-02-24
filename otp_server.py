d1c3294785bc2898b5d8816a3fe8b763cd5dabc7
Matt Bernard <commerical@pm.me>
2021-02-23 19:57:45 -0500

    Removed server code and print info to terminal window

--- otp_server.py
+++ otp_server.py
@@ -2,13 +2,13 @@
 """
 Generate One Time Pad files, or serve the pads up using a CherryPy server.
 """
-from jinja2 import DictLoader, Environment
+#from jinja2 import DictLoader, Environment
 import secrets
-import cherrypy
+#import cherrypy
 
 
 # CherryPy config
-CONFIG = {'global': {'server.socket_port': 8080}}
+#CONFIG = {'global': {'server.socket_port': 8080}}
 
 # The characters available for the One Time Pad
 OTP_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -42,59 +42,58 @@ def generate_message(row_count=ROW_COUNT, group_count=GROUP_COUNT, group_size=GR
     return '\n'.join(generate_row(group_count, group_size) for _ in range(ROW_COUNT))
 
 
-class Root(object):
+"""
+Put all messages into the page
+"""
+messages = (generate_message() for _ in range(MESSAGE_COUNT))
 
-    @cherrypy.expose
-    def one_time_pad(self):
-        """
-        Put all messages into the page
-        """
-        messages = (generate_message() for _ in range(MESSAGE_COUNT))
-        template = env.get_template('one_time_pad')
-        return template.render(messages=messages)
+for m in messages:
+    print(m)
+# template = env.get_template('one_time_pad')
+# return template.render(messages=messages)
 
 
 # This is the only page.  Links are shortened to make manual typing easier.
-templates = {'one_time_pad': '''
-<title>One Time Pad - Unique, just for you</title>
-<style>
-/* Remove decoration from links so they are readable when printed */
-a { text-decoration: none; }
-</style>
-[[ for num, message in messages|enumerate(1) ]]
-<pre>Message [- num -]</pre>
-<pre>
-[- message -]
-</pre>
-[[ endfor ]]
+# templates = {'one_time_pad': '''
+# <title>One Time Pad - Unique, just for you</title>
+# <style>
+# /* Remove decoration from links so they are readable when printed */
+# a { text-decoration: none; }
+# </style>
+# [[ for num, message in messages|enumerate(1) ]]
+# <pre>Message [- num -]</pre>
+# <pre>
+# [- message -]
+# </pre>
+# [[ endfor ]]
 
-Print this page and distribute the copies (along with the <a href="https://lrnsr.co/aY6m">One Time Pad Cheat Sheet
-https://lrnsr.co/aY6m</a>) to all members of your group that you trust to receive your encrypted messages.  Every
-person must have their OWN copy of this "One Time Pad" to encrypt and decrypt messages.
-<br>
-<b>Use each message ONLY ONCE.</b>  Cut off and burn each message from this paper as it is used.
-<br>
-If you want more One Time Pads, simply <a href=".">go here to refresh the page:
-https://learningselfreliance.com/one_time_pad</a>.  The server will generate a unique page just for you. This page
-is not stored on the server, and cannot be retrieved once you close this window!
-<br>
-To learn how to use this page, please visit: <a href="https://lrnsr.co/H7Za">https://lrnsr.co/H7Za</a>
-''',
-}
+# Print this page and distribute the copies (along with the <a href="https://lrnsr.co/aY6m">One Time Pad Cheat Sheet
+# https://lrnsr.co/aY6m</a>) to all members of your group that you trust to receive your encrypted messages.  Every
+# person must have their OWN copy of this "One Time Pad" to encrypt and decrypt messages.
+# <br>
+# <b>Use each message ONLY ONCE.</b>  Cut off and burn each message from this paper as it is used.
+# <br>
+# If you want more One Time Pads, simply <a href=".">go here to refresh the page:
+# https://learningselfreliance.com/one_time_pad</a>.  The server will generate a unique page just for you. This page
+# is not stored on the server, and cannot be retrieved once you close this window!
+# <br>
+# To learn how to use this page, please visit: <a href="https://lrnsr.co/H7Za">https://lrnsr.co/H7Za</a>
+# ''',
+# }
 
-env = Environment(
-    loader=DictLoader(templates),
-    autoescape=True,
-    block_start_string='[[',
-    block_end_string=']]',
-    comment_start_string='[#',
-    comment_end_string='#]',
-    variable_start_string='[-',
-    variable_end_string='-]',
-)
+# env = Environment(
+#     loader=DictLoader(templates),
+#     autoescape=True,
+#     block_start_string='[[',
+#     block_end_string=']]',
+#     comment_start_string='[#',
+#     comment_end_string='#]',
+#     variable_start_string='[-',
+#     variable_end_string='-]',
+# )
 
-env.filters['enumerate'] = enumerate
+# env.filters['enumerate'] = enumerate
 
-if __name__ == '__main__':
-    # Start cherrypy server
-    cherrypy.quickstart(cherrypy.Application(Root()), '/', config=CONFIG)
+# if __name__ == '__main__':
+#     # Start cherrypy server
+#     ##cherrypy.quickstart(cherrypy.Application(Root()), '/', config=CONFIG)
